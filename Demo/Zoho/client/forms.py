5e63fe555a5e51eb9624523f80d6b656cd735138
Swatij123 <zcon.swatij@gmail.com>
2023-07-21 12:15:06 +0530

    latest version

--- Demo/Zoho/client/forms.py
+++ Demo/Zoho/client/forms.py
@@ -2,13 +2,15 @@ from msilib.schema import tables
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from numpy import require
 from .models import Client, ClientContact
 
-Currency_Choice =[('INR','INR'),
-           ('USD','USD'),
-           ('EUR','EUR'),
-            ('JPY', 'JPY'),
-            ('AUD','AUD'),
+Currency_Choice =[('AUD','AUD'),
+                  ('EUR','EUR'),
+                 ('INR','INR'),           
+                 ('JPY', 'JPY'),
+                 ('USD','USD'),
+            
 ]
 
 Billing_Choice =[('Hourly Job Rate','Hourly Job Rate'),
@@ -18,15 +20,21 @@ Billing_Choice =[('Hourly Job Rate','Hourly Job Rate'),
 
 ]
 class clientContact(forms.ModelForm):
+    
+    Emailid=forms.EmailField(label='Email Id')
+    FirstName=forms.CharField(label='First Name')
+    LastName=forms.CharField(label='Last Name')
+    
     class Meta:
         model = ClientContact
         fields = ['ClientName','Emailid','FirstName','LastName','Phone','Mobile','Fax']
         
+        
 
 class client(forms.ModelForm):
-    ClientName= forms.CharField(max_length=30)
+    ClientName= forms.CharField(label='Client Name',max_length=30)
     Currency= forms.CharField(label='Currency',widget=forms.Select(choices=Currency_Choice))
-    BillingMethod= forms.CharField(label='Currency',widget=forms.Select(choices=Billing_Choice))
+    BillingMethod= forms.CharField(label='Billing Method',widget=forms.Select(choices=Billing_Choice))
     class Meta:
         model =Client
         fields = ['ClientName','Currency','BillingMethod']
